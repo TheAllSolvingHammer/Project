a74fa1754b50d7fc2b4a899eafeaa166b4381c4f
Danny <kilatomerat@gmail.com>
2024-02-27 18:28:35 +0200

    Initial Commit !

--- src/sit/tu_varna/bg/project/Main.java
+++ src/sit/tu_varna/bg/project/Main.java
@@ -1,10 +1,13 @@
 package sit.tu_varna.bg.project;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Test.printSmth();
         //dddddddddddddddddddddddddddddddddddddddddd
+        System.out.println("Testing if git works");
     }
 }
