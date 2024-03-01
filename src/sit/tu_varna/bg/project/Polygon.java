4286feeeedf67c0bdcb09339465b7335317f2fea
Danny <kilatomerat@gmail.com>
2024-03-01 21:52:48 +0200

    Commit 1st Week

---
+++ src/sit/tu_varna/bg/project/Polygon.java
@@ -0,0 +1,10 @@
+package sit.tu_varna.bg.project;
+
+public class Polygon {
+    //shte razgledam sluchaq za nasledqvaneto
+    private double[] coords;
+
+    public Polygon(double[] coords) {
+        this.coords = coords;
+    }
+}
