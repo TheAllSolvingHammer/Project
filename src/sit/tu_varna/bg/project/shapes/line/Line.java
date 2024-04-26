89091716e3fa76918a606e4d0b3472d23124bb42
Danny <kilatomerat@gmail.com>
2024-04-26 18:17:44 +0300

    Commit 10th week added ShapeManager and adjusted Polyline.class Polygon.class and Line.class

--- src/sit/tu_varna/bg/project/shapes/line/Line.java
+++ src/sit/tu_varna/bg/project/shapes/line/Line.java
@@ -1,56 +1,48 @@
 package sit.tu_varna.bg.project.shapes.line;
 
-public class Line {
+import sit.tu_varna.bg.project.colors.NamedColors;
+import sit.tu_varna.bg.project.shapes.Shape;
+
+public class Line implements Shape {
+    private String name;
     private double startX;
     private double startY;
     private double endX;
     private double endY;
     private double strokeWidth;
+    private NamedColors color;
 
-    public Line(double startX, double startY, double endX, double endY) {
+    public Line(String name, double startX, double startY, double endX, double endY, double strokeWidth, NamedColors color) {
+        this.name = name;
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-    }
-
-    public double getStartX() {
-        return startX;
-    }
-
-    public void setStartX(double startX) {
-        this.startX = startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public void setStartY(double startY) {
-        this.startY = startY;
-    }
-
-    public double getEndX() {
-        return endX;
-    }
-
-    public void setEndX(double endX) {
-        this.endX = endX;
-    }
-
-    public double getEndY() {
-        return endY;
-    }
-
-    public void setEndY(double endY) {
-        this.endY = endY;
-    }
-
-    public double getStrokeWidth() {
-        return strokeWidth;
-    }
-
-    public void setStrokeWidth(double strokeWidth) {
         this.strokeWidth = strokeWidth;
+        this.color = color;
     }
+
+    public Line(double startX, double startY, double endX, double endY, double strokeWidth, NamedColors color) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+        this.strokeWidth = strokeWidth;
+        this.color = color;
+        this.name=null;
+    }
+
+    @Override
+    public String convertIntoString() {
+
+        if(name==null || name.isEmpty()){
+            return "<line stroke-width=\""+strokeWidth+"\" stroke=\""+color+"\" x1=\""+startX+"\" y1=\""+startY+"\" x2=\""+endX+"\" y2=\""+endY+"\" />";
+        }
+        else {
+            return "<line name=\""+name+"\" stroke-width=\"" + strokeWidth + "\" stroke=\"" + color + "\" x1=\"" + startX + "\" y1=\"" + startY + "\" x2=\"" + endX + "\" y2=\"" + endY + "\" />";
+        }
+
+
+    }
+
 }
